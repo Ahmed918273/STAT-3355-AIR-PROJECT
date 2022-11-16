cbc771fdf6091169d59b37951e29ef50665890b0
Ahmed918273 <ahmedghabin@gmail.com>
2022-11-15 22:56:32 -0600

    Ahmed update

--- Question 1/Cleaning.R
+++ Question 1/Cleaning.R
@@ -1,5 +1,3 @@
-# Cleaning for question 1
-
 # import library
 library(tidyverse)
 library(readxl)
@@ -122,3 +120,120 @@ accident_long_lat$latitude <- as.numeric(accident_long_lat$latitude)
 accident_long_lat <- accident_long_lat[-c(1:19), ]
 
 rm(list = c("i", "longitude", "latitude"))
+
+
+# Getting map data
+main_states <- map_data("state")
+
+# State population data
+state_population <- read.csv("https://raw.githubusercontent.com/ds4stats/r-tutorials/master/intro-maps/data/StatePopulation.csv", as.is = TRUE) # nolint
+
+# Create a new coloumn in the state_population data frame called "percentage"
+state_population$percentage <- (state_population$population /
+sum(state_population$population)) * 100
+
+# Create a new coloumn in the state_population data frame called "abbreviation"
+state_population$abbreviation <- tolower(state_population$region)
+
+# Create a list of all abbreviations of the states capitalised
+state_abbreviations <- c("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE",
+"FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+"MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY",
+"NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT",
+"VT", "VA", "WA", "WV", "WI", "WY")
+
+# Sort the state_population data frame by the region column
+state_population <- state_population[order(state_population$region), ]
+
+# Add the state_abbreviations list to the state_population data
+#frame in the abbreviation column
+state_population$abbreviation <- state_abbreviations
+
+# Creating new data
+merged_states <- inner_join(main_states, state_population, by = "region")
+
+# Table the long_lat_events_all data frame
+crash_table <- table(long_lat_events_all$ev_state)
+
+# Turn the table into a data frame
+crash_df <- as.data.frame(crash_table)
+
+# Reorder the crash_df descending
+crash_df <- crash_df[order(crash_df$Freq, decreasing = TRUE), ]
+
+# Create a new column in crash_df called "Percentage" and input the percentage
+crash_df$Percentage <- round(crash_df$Freq / sum(crash_df$Freq) * 100, 2)
+
+# Merge the crash_df with the state_population data frame
+crash_df_merged <- merge(state_population, crash_df)
+
+# Only keep rows in which Var1 and region are the same
+crash_df_merged <- crash_df_merged[crash_df_merged$Var1 == crash_df_merged$abbreviation, ]
+
+# Rename the Freq column to "Accidents"
+colnames(crash_df_merged)[2] <- "Accidents"
+
+# Rename the percentage column to "Percentage_Population"
+colnames(crash_df_merged)[4] <- "Percentage_Popluation"
+
+# Rename the percentage column to "Percentage_Population"
+colnames(crash_df_merged)[8] <- "Percentage_Accidents"
+
+# Manipulating data to make a bar plot
+
+# Create a new data frame called "crash_df_merged2"
+# from the crash_df_merged data frame
+crash_df_merged2 <- crash_df_merged
+
+# Remove the region column
+crash_df_merged2 <- crash_df_merged2[, -1]
+
+# Remove the elect_votes column
+crash_df_merged2 <- crash_df_merged2[, -2]
+
+# Make abbreviations the first column in the crash_df_merged2 data frame
+crash_df_merged2 <- crash_df_merged2[, c("abbreviation", "Accidents",
+ "Percentage_Popluation", "Var1", "Freq", "Percentage_Accidents")]
+
+# Add a column to the end named "Percentage_Type" and have it be filled with "Y"
+crash_df_merged2$Percentage_Type1 <- "Accidents"
+
+# Add a column to the end named "Percentage_Type" and have it be filled with "Y"
+crash_df_merged2$Percentage_Type2 <- "Population"
+
+# Create a new data frame called "crash_df_merged3" in which columns
+# 4, 5, 6, and 7 are taken from the crash_df_merged2 data frame
+crash_df_merged3 <- crash_df_merged2[, c(4, 5, 6, 7)]
+
+# Create a new data frame called "crash_df_merged4" in which columns
+# 1, 2, 3, and 8 are taken from the crash_df_merged2 data frame
+crash_df_merged4 <- crash_df_merged2[, c(1, 2, 3, 8)]
+
+# Rename the columns in crash_df_merged3 to "State", "Number", "Percentage", and "Percentage_Type"
+colnames(crash_df_merged3) <- c("State", "Number", "Percentage", "Percentage_Type")
+
+# Only include the top 10 states in the crash_df_merged3 data frame
+crash_df_merged3 <- crash_df_merged3[1:10, ]
+
+# Rename the columns in crash_df_merged4 to "State", "Number", "Percentage", and "Percentage_Type"
+colnames(crash_df_merged4) <- c("State", "Number", "Percentage", "Percentage_Type")
+
+# Only include the top 10 states in the crash_df_merged4 data frame
+crash_df_merged4 <- crash_df_merged4[1:10, ]
+
+# Combine the crash_df_merged3 and crash_df_merged4 data frames
+crash_df_merged5 <- rbind(crash_df_merged3, crash_df_merged4)
+
+# create a bar plot using the crash_df_merged5 data frame
+bar_plot_two <- ggplot(crash_df_merged5, 
+  aes(x = reorder(State, -Percentage), 
+  y = Percentage, fill = Percentage_Type)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Percentage of Accidents and Population by State",
+  x = "State", y = "Percentage", fill = "Percentage Type")
+
+# Only include the top 10 rows based on Freq
+crash_df_merged <- crash_df_merged[1:10, ]
+
+# Only include the top 10 rows based on Freq
+crash_df <- crash_df[1:50, ]
